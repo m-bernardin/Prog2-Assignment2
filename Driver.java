4e3f994e0c22cbb31daae7d73c23adb713065ae0
m-bernardin <mjrb4mjrb@gmail.com>
2026-03-23 11:52:08 -0400

    driver start

--- Driver.java
+++ Driver.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Driver{
-    
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        boolean running=true;
+        ArrayList<Scientist> scientists=new ArrayList<>();
+        ArrayList<Project> projects=new ArrayList<>();
+        while(running){
+            System.out.print("Please select an option\n1. Create project\n2. Add scientist\n3. Show all info\n4. Search for a scientist\n5. Remove project\n6. Remove scientist\n7. quit\n> ");
+        }
+    }
 }
